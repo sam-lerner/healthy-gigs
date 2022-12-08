2f8b57b60725958de33ca196cafa96be8c5b8ccd
Blanket <jaketung2000@gmail.com>
2022-12-08 11:32:50 -0500

    api call var storage functional

--- assets/javascript/jake -apicall.js
+++ assets/javascript/jake -apicall.js
@@ -13,18 +13,20 @@ console.log("https://service.zipapi.us/zipcode/county/"+ zip +"/?X-API-KEY=js-9b
 }
 
 function cdcCovidData(state, county) {
-  // console.log('https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state='+ state +'&county='+ county +'%20County');
+  var urlTest = "https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state="+ state +"&county="+ county +"%20County";
   $.ajax({
-    url:"https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state="+ state +"&county="+ county +"%20County",
+    url:urlTest,
     method: "GET",
   }).then(function (response) {
     console.log("Ajax Reponse \n-------------");
     console.log(response);
-    level = response.covid_19_community_level;
-    covidCase = response.covid_cases_per_100k;
-    pop = response.county_population;
-    updateDay = response.date_updated;
+    level = response[0].covid_19_community_level;
+    covidCase = response[0].covid_cases_per_100k;
+    pop = response[0].county_population;
+    updateDay = response[0].date_updated;
+   console.log(level);
   });
+ 
   return;
 }
 
@@ -36,4 +38,5 @@ county ="Ocean";
 geoPostCode(postalCode);
 console.log(county,state,postalCode);
 cdcCovidData(state, county);
+console.log(pop,covidCase,level);
 
