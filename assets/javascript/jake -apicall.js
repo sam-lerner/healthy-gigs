73fe2baa6d13617297e91acd5d8c5b7e2602b029
Blanket <jaketung2000@gmail.com>
2022-12-08 04:18:39 -0500

    recieved response from both api and setup variables, issue - can not pass values of county from api

--- assets/javascript/jake -apicall.js
+++ assets/javascript/jake -apicall.js
@@ -1,17 +1,39 @@
-
-var county = 'Ocean'
-var state = 'New Jersey'
-
-function call() {
-    console.log('https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state='+ state +'&county='+ county +'%20County');
-$.ajax({
-    url: 'https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state='+ state +'&county='+ county +'%20County',
-    method: 'GET',
+function geoPostCode(zip) {
+console.log("https://service.zipapi.us/zipcode/county/"+ zip +"/?X-API-KEY=js-9bba29279d7363655cc244b9ad8465ee");
+  $.ajax({
+    url: "https://service.zipapi.us/zipcode/county/"+ zip +"/?X-API-KEY=js-9bba29279d7363655cc244b9ad8465ee",
+    method: "GET",
   }).then(function (response) {
-    console.log('Ajax Reponse \n-------------');
+    console.log("Ajax Reponse \n-------------");
     console.log(response);
+    county = response.data.county;
+
   });
-return;
+  return;
 }
 
-call();
+function cdcCovidData(state, county) {
+  // console.log('https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state='+ state +'&county='+ county +'%20County');
+  $.ajax({
+    url:"https://data.cdc.gov/resource/3nnm-4jni.json?$order=date_updated%20DESC&$limit=1&state="+ state +"&county="+ county +"%20County",
+    method: "GET",
+  }).then(function (response) {
+    console.log("Ajax Reponse \n-------------");
+    console.log(response);
+    level = response.covid_19_community_level;
+    covidCase = response.covid_cases_per_100k;
+    pop = response.county_population;
+    updateDay = response.date_updated;
+  });
+  return;
+}
+
+
+var county, state, postalCode, level, covidCase, pop, updateDay;
+postalCode = '08753';
+state = "New Jersey";
+county ="Ocean";
+geoPostCode(postalCode);
+console.log(county,state,postalCode);
+cdcCovidData(state, county);
+
